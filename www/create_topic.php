8485303dd62379e6690e834ff69f0fe8b28387cd
nebroskiy <tatarinov0123@gmail.com>
2022-04-25 19:34:00 +0300

    изменено:      logs/nginx/project_access.log 	изменено:      mariadb/ib_logfile0 	изменено:      www/create_topic.php 	изменено:      www/pdo/data_insert.php 	www/html/create_topic_form.php 	www/html/display_topics_fill.php 	www/pdo/data_topic_check.php 	www/pdo/topics_fetch_id_desc.php

--- www/create_topic.php
+++ www/create_topic.php
@@ -2,23 +2,14 @@
     $article = "Create Topic";
     require "./html/html-head.php";
 ?>
-<body>
 <header>
     <h1>Create Topic</h1>
 </header>
 <?php
     require "./html/nav_main.php";
     require "./html/nav_blog.php";
+    require "./html/create_topic_form.php";
 ?>
-</body>
-<div class="container mt-4">
-    <form action="/pdo/topic_data.php" method="post">
-        <input type="text" name="title" placeholder="Title..." class="form-control"><br>
-        <input type="text" name="description" placeholder="Description..." class="form-control"><br>
-        <textarea name="topic" placeholder="Your topic..." class="form-control"></textarea><br>
-        <input type="submit" value="Submit" class="btn btn-success"><br>
-    </form>
-</div>
 <?php
     require "./html/html-footer.php";
 ?>
--- www/pdo/data_insert.php
+++ www/pdo/data_insert.php
@@ -1,23 +1,13 @@
 <?php
 function data_insert ($title, $description, $topic)
 {
-    if (strlen($title) <= 5) {
-        echo "This title is too short.";;
-    } elseif (trim($title) == "" || $title == "") {
-        echo "Enter title";
-    } elseif (strlen($description) <= 5) {
-        echo "Description is too short.";
-    } elseif (trim($description) == "" || $description == "") {
-        echo "Enter description";
-    } elseif (strlen($topic) < 10) {
-        echo "Your topic is too short.";
-    } elseif (trim($topic) == "" || $topic == "") {
-        echo "Enter topic";
-    } else {
+    require "data_topic_check.php";
+    $data_topic_check = data_topic_check($title, $description, $topic);
+    if ($data_topic_check){
         require "pdo_connection.php";
         $connection = connection($driver, $host, $dbname, $charset, $username, $password);
         require "data_insert_mariadb.php";
-                data_insert_mariadb($connection, $title, $description, $topic);
+        data_insert_mariadb($connection, $title, $description, $topic);
         header("Location:/");
     }
 }
